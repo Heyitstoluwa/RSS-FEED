3470fb80ba2da692a0072be8b0209a229a9af2a7
Emmanuel <yungteezie3@gmail.com>
2023-07-13 00:39:44 +0100

    Post images collected

--- RSS-FEED.py
+++ RSS-FEED.py
@@ -12,26 +12,28 @@ for entry in feed.entries:
     post_title = entry.title
     post_url = entry.link
     post_author = entry.author
-    image_url = entry.url
-
+   
 
 
     # Access the post webpage
     response = requests.get(post_url)
     post_html = response.text
-
+    
     # Parse the post content
     soup = BeautifulSoup(post_html, 'html.parser')
     post_content = soup.find('div', class_='post-content')  # Replace with the appropriate HTML element that contains the post content
+    div_element = soup.find('div', class_='post-image-wrapper')
+
 
     if post_content:
         # Process the content as needed
         full_content = post_content.get_text()
-    
+        img_tag = div_element.find("img")
+        img_src = img_tag["src"]
 
         # Print or do something with the full content
         print(f"Title: {post_title}")
         print(f"Full Content: {full_content}")
         print(f"Author: {post_author}")
-        print(f"src: {image_url}")
-        print("************************************************************************************************************************************")
+        print(f"Featured Image: {img_src}")
+        print("********************************************************")
